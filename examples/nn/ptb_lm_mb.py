808b82a1029b908ec280b39648c8bb12cecd3172
Qipeng Guo <qpguo12@fudan.edu.cn>
2017-04-15 13:25:33 +0800

    add parser model builder version

--- examples/nn/ptb_lm_mb.py
+++ examples/nn/ptb_lm_mb.py
@@ -1,19 +1,19 @@
 import numpy.random as RNG
+import numpy as NP
 import minpy.numpy as np
 from minpy.nn.model_builder import *
 from minpy.nn.modules import *
+class LMModel(Model):
+    def __init__(self, vocab_size=10000, H_DIM =200, EMB_DIM=200):
+        super(LMModel, self).__init__(loss='softmax_loss') # softmax don't support index target 
 
-def RNNModel(Model):
-    def __init__(self, H_DIM, vocab_size, EMB_DIM):
-        super(RNNModel, self).__init__(loss='softmax_loss')
-
-        self._embedding = Embedding(voab_size, EMB_DIM) # need to implement 
+        self._embedding = Embedding(input_dim=vocab_size, output_dim=EMB_DIM) 
         self._rnn = RNN(H_DIM, 'tanh')
         self._linear = FullyConnected(num_hidden=H_DIM)
 
     def forward(self, data, mode='training'):
         embs = self._embedding(data)
-        N, length, D = embs
+        N, length, D = embs.shape
 
         hm1 = None
         hs = []
@@ -25,24 +25,25 @@ def RNNModel(Model):
         hs = np.stack(hs, axis=1)
         return self._linear(hs.reshape((N*length, D))).reshape((N,length,D))
 
+unpack_batch = lambda batch : (batch.data[0], batch.label[0])
 
-if __name__ = '__main__':
+if __name__ == '__main__':
 
     vocab_size= 10000
     seq_len = 35
     num_samples = 5000
     train_X = RNG.randint(vocab_size, size=(num_samples, seq_len))
-    train_Y = np.concatenate([train_X[:, 1:], np.zeros((num_samples, 1))], axis=1)
+    train_Y = NP.concatenate([train_X[:, 1:], NP.zeros((num_samples, 1))], axis=1) # raise an error if we use minpy.numpy
     test_X = RNG.randint(vocab_size, size=(num_samples, seq_len))
-    test_Y = np.concatenate([test_X[:, 1:], np.zeros((num_samples, 1))], axis=1)
+    test_Y = NP.concatenate([test_X[:, 1:], NP.zeros((num_samples, 1))], axis=1)
 
     batch_size = 64
 
     from minpy.nn.io import NDArrayIter
     train_data_iter = NDArrayIter(train_X, train_Y, batch_size=batch_size, shuffle=True)
-    test_data_iter = NDArrayIter([est_X, test_Y, batch_size=batch_size, shuffle=False)
+    test_data_iter = NDArrayIter(test_X, test_Y, batch_size=batch_size, shuffle=False)
 
-    model = RNNModel(128, vocab_size, 200)
+    model = LMModel(vocab_size=vocab_size, H_DIM=200, EMB_DIM=200)
     updater = Updater(model, update_rule='rmsprop', learning_rate=0.001)
 
 
